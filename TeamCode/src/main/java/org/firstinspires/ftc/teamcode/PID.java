bc5643c0a83e08fd17b21cde37bed8d0ee361f35
Peter <47194437+Petelax@users.noreply.github.com>
2019-10-23 15:35:35 -0400

    Added 90 Degree PID turn

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID.java
@@ -4,24 +4,32 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "move-y teleop")
-public class OperatorDrive extends OpMode {
+import java.util.concurrent.CountDownLatch;
+
+@TeleOp(name = "PID")
+public class PID extends OpMode {
 
     private DcMotor left;
     private DcMotor right;
+
+    private int oldErrLeft;
+    private int oldErrRight;
+
+    private final double COUNTS_PER_INCH = (1440/(3.1415*4));
 
     @Override
     public void init() {
         left = hardwareMap.get(DcMotor.class, "right_drive");
         right = hardwareMap.get(DcMotor.class, "left_drive");
 
-        left.setMode(DcMotor.RunMode.RESET_ENCODERS);
-        right.setMode(DcMotor.RunMode.RESET_ENCODERS);
+        left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         right.setDirection(DcMotorSimple.Direction.REVERSE);
-
 
     }
 
@@ -30,17 +38,37 @@ public class OperatorDrive extends OpMode {
         int leftPosition = left.getCurrentPosition();
         int rightPosition = right.getCurrentPosition();
 
-        double averagePosition = (leftPosition + rightPosition) / 2;
-        double setpoint = 0;
-        double error = setpoint - averagePosition;
-        double kP = 0.01;
+        double setpoint = 24;
 
-        left.setPower(error * kP);
-        right.setPower(error * kP);
+//        left.setTargetPosition((int) (setpoint * COUNTS_PER_INCH));
+//        right.setTargetPosition((int) (setpoint * COUNTS_PER_INCH));
+//
+//        left.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//        right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//
+//        left.setPower(1);
+//        right.setPower(1);
+
+        turning90cw();
+
+//        oldErrLeft = (int)(((13 * 3.1415) / 4) * COUNTS_PER_INCH) - leftPosition;
+//        oldErrRight = -(int)(((13 * 3.1415) / 4) * COUNTS_PER_INCH) - rightPosition;
 
         telemetry.addData("Status", "Running");
         telemetry.addData("Left Motor Position", leftPosition);
         telemetry.addData("Right Motor Position", rightPosition);
         telemetry.update();
+    }
+
+    private void turning90cw() {
+
+        left.setTargetPosition((int)(((13 * 3.1415) / 4) * COUNTS_PER_INCH));
+        right.setTargetPosition(-(int)(((13 * 3.1415) / 4) * COUNTS_PER_INCH));
+
+        left.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        left.setPower(0.5);
+        right.setPower(-0.5);
     }
 }
