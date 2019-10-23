f36f83daedaaeff7952692774125ad5ee5553095
Aidan Zerwer <robobug9@gmail.com>
2019-10-23 15:42:23 -0400

    Auto setup

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name="Autonomous")
+public class Auto extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        waitForStart();
+        runtime.reset();
+    }
+}
